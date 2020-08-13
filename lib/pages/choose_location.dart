99be11b817beca679852182a374fef73617bc5ce
MbuguaCaleb <mbuguacaleb30@gmail.com>
2020-08-13 13:24:32 +0300

    'asynchronous-code-in-flutter'

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,11 +6,32 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
+  /*Void means  that the function returns nothing*/
+
+    void getData() async{
+
+    // simulating network request for username
+    //Duration object takes in two arguments.
+    //Duration is very similar to setTimeOut in Javascript
+    String username =await Future.delayed(Duration(seconds: 3), (){
+      return 'Caleb';
+    });
+
+    //simulate network request to get Bio of the username
+    String bio = await Future.delayed(Duration(seconds: 2), (){
+      return 'guitarist, programming Ninja & Born again';
+    });
+
+    print('$username - $bio');
+  }
+
   int counter =0;
   @override
   void initState() {
+    print('initState lifecycle method ran');
     super.initState();
-    print('initState function ran');
+    getData();
+    print('Hey there!');
   }
   @override
   Widget build(BuildContext context) {
